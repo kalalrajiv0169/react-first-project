fb24f1b814562145c24f047cd2b6463fd62a06c4
KALAL RAJIV <153971894+kalalrajiv0169@users.noreply.github.com>
2025-05-30 18:34:33 +0530

    v1
    v1

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> <h1>react first project hello evryone</h1>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+          <a
+          className="App-link"
+          href="https://github.com/kalalrajiv0169"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
